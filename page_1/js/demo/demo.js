3c9d6e877e3964a0bea5d3df4ea829f1d306b61a
sam urmiahchi <92691798+samiiiiiiur@users.noreply.github.com>
2023-03-11 23:40:33 +0300

    Update demo.js

--- page_1/js/demo/demo.js
+++ page_1/js/demo/demo.js
@@ -1,13 +1,4 @@
-/*
- * blueimp Gallery Demo JS
- * https://github.com/blueimp/Gallery
- *
- * Copyright 2013, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * https://opensource.org/licenses/MIT
- */
+
 
 /* global blueimp, $ */
 
